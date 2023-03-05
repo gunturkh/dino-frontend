0f33aa57495925af20a2f0d6033c4425e4ae743e
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2023-03-06 00:48:21 +0700

    feat: - add example for using html inside canvas - make the background blurry

--- src/AppCanvas.tsx
+++ src/AppCanvas.tsx
@@ -1,18 +1,65 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, useGLTF, } from '@react-three/drei'
-import { useLoader } from '@react-three/fiber'
-
+import React, { Suspense, useMemo, useRef } from 'react'
+import { useGLTF, Html, shaderMaterial, useTexture, Plane } from '@react-three/drei'
+import { Canvas, useLoader, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
+import { EffectComposer, DepthOfField, Bloom } from "@react-three/postprocessing";
+
 import ForestBg from './assets/image/forest_background.jpg'
 
-function Image() {
-  const texture = useLoader(THREE.TextureLoader, ForestBg)
+function BlurredBackground() {
+  const planeRef = useRef();
+  const map = useTexture(ForestBg)
+  const { viewport } = useThree();
+
+
+  // useFrame(({ camera }) => {
+  //   // update the plane's position to always face the camera
+  //   // @ts-ignore
+  //   planeRef?.current?.lookAt(camera.position);
+  // });
+
   return (
-    <mesh>
-      <planeBufferGeometry attach="geometry" args={[20, 10]} />
-      <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
-    </mesh>
+    <>
+      <Plane
+        ref={planeRef}
+        // position={[0, 0, -5]}
+        args={[16, 10]}
+        // rotation={[Math.PI / 2, 0, 0]}
+      >
+        <meshBasicMaterial attach="material" map={map} transparent opacity={0.5} toneMapped={false}/>
+      </Plane>
+      <EffectComposer>
+        <DepthOfField
+          focusDistance={0}
+          focalLength={0.02}
+          bokehScale={8}
+          height={viewport?.height}
+        />
+        <Bloom 
+        luminanceThreshold={0} 
+        luminanceSmoothing={0.6} 
+        height={viewport.height} 
+        opacity={1}  
+        />
+      </EffectComposer>
+    </>
+  );
+}
+
+
+const signUpForm = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      width: '12vw',
+      height: '40vh',
+      backgroundColor: 'white',
+    }}>
+      <label htmlFor="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required />
+    </div>
   )
 }
 
@@ -20,11 +67,14 @@ export default function AppCanvas() {
   return (
     // canvas full screen
     <div style={{ width: "100vw", height: "100vh" }}>
-    <Canvas flat linear eventPrefix="client" camera={{ position: [0, 0, 5] }}>
-      <Suspense fallback={null}>
-        <Image />
+      <Canvas flat linear eventPrefix="client">
+        <Suspense fallback={null}>
+          <BlurredBackground />
+          <Html transform>
+            {signUpForm()}
+          </Html>
         </Suspense>
       </Canvas>
-      </div>
+    </div>
   )
 }
