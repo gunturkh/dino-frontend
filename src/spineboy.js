cf95f4c88a1692c514288c90d203c7d73c862725
Guntur KH <guntur.kh@gmail.com>
2023-03-08 02:58:07 +0700

    feat: add dragon

--- src/spineboy.js
+++ src/spineboy.js
@@ -5,11 +5,7 @@ import TextInput from "pixi-text-input";
 
 function generateBox(w, h, state) {
   var box = new PIXI.Container();
-  var sprite = new PIXI.TilingSprite(
-    PIXI.Texture.from("tile.png"),
-    w,
-    h
-  );
+  var sprite = new PIXI.TilingSprite(PIXI.Texture.from("tile.png"), w, h);
   var mask = new PIXI.Graphics();
 
   mask.beginFill(0);
@@ -46,7 +42,7 @@ function Spineboy() {
     const animation = new Spine(resource.spineData);
 
     // set the position
-    animation.x = app.screen.width / 2;
+    animation.x = app.screen.width / 5;
     animation.y = app.screen.height;
 
     animation.scale.set(1);
@@ -106,10 +102,10 @@ function Spineboy() {
 
     app.stage.addChild(animation);
 
-    app.stage.on("pointerdown", () => {
-      animation.state.setAnimation(0, "jump", false);
-      animation.state.addAnimation(0, "walk", true, 0);
-    });
+    // app.stage.on("pointerdown", () => {
+    //   animation.state.setAnimation(0, "jump", false);
+    //   animation.state.addAnimation(0, "walk", true, 0);
+    // });
 
     // if (animation.state.hasAnimation("walk")) {
     //   // run forever, little boy!
@@ -120,7 +116,45 @@ function Spineboy() {
     //   animation.autoUpdate = true;
     // }
   });
+  PIXI.Assets.load("dragon/dragon.json").then(onAssetsLoaded);
 
+  function onAssetsLoaded(dragonAsset) {
+    // instantiate the spine animation
+    const dragon = new Spine(dragonAsset.spineData);
+    dragon.skeleton.setToSetupPose();
+    dragon.update(0);
+    dragon.autoUpdate = false;
+
+    // create a container for the spine animation and add the animation to it
+    const dragonCage = new PIXI.Container();
+    dragonCage.addChild(dragon);
+
+    // measure the spine animation and position it inside its container to align it to the origin
+    const localRect = dragon.getLocalBounds();
+    dragon.position.set(-localRect.x, -localRect.y);
+
+    // now we can scale, position and rotate the container as any other display object
+    const scale = Math.min(
+      (app.screen.width * 0.7) / dragonCage.width,
+      (app.screen.height * 0.7) / dragonCage.height
+    );
+    dragonCage.scale.set(scale, scale);
+    dragonCage.position.set(
+      (app.screen.width - dragonCage.width) ,
+      (app.screen.height - dragonCage.height)
+    );
+
+    // add the container to the stage
+    app.stage.addChild(dragonCage);
+
+    // once position and scaled, set the animation to play
+    dragon.state.setAnimation(0, "flying", true);
+
+    app.ticker.add(() => {
+      // update the spine animation, only needed if dragon.autoupdate is set to false
+      dragon.update(app.ticker.deltaMS / 1000); // IN SECONDS!
+    });
+  }
   return <div />;
 }
 
