c99ae8d2247d52c8dfa11969a1cd593b188f85c7
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2023-03-16 15:31:18 +0700

    chore: example change scene

--- src/AppNew.tsx
+++ src/AppNew.tsx
@@ -1,43 +1,65 @@
 import * as PIXI from "pixi.js";
 import {Stage, Container, Sprite, Text, useApp, AppProvider } from "@pixi/react";
-import { useMemo } from "react";
+import { Input } from "@pixi/ui";
+import { useMemo, useState } from "react";
 
 
 
 export const AppNew = () => {
+  const [scene, setScene] = useState('HOME')
+
+
+
   const options = {
     backgroundColor: 0x1099bb,
-    // antialias: true,
-    // autoresize: true,
-    // autoStart: false,
-    // clearBeforeRender: false,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    antialias: true,
+    autoresize: true,
+    autoStart: false,
+    clearBeforeRender: false,
   };
 
   // const app = useApp();
+
+  const appWidth = window.innerWidth;
+  const appHeight = window.innerHeight;
 
 
   const blurFilter = useMemo(() => new PIXI.BlurFilter(4), []);
     return (
       // <AppProvider value={app} >
-        <Stage options={options} >
-          <Container width={window.innerWidth} height={window.innerHeight}>
+                <>
+        <button onClick={() => setScene('GAME')}>Start Game</button><Stage width={appWidth} height={appHeight} options={options}>
 
-                  <Sprite 
-                    image={'image/forest_background.jpg'}
-                    width={window.innerWidth}
-                    height={window.innerHeight}
-                    // anchor={{ x: 0.5, y: 0.5 }}
-                    // x={0}
-                    // y={0}
-                  />
-                <Container x={400} y={330}>
-        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
-      </Container>
-      </Container>
+          {scene === 'GAME' && (
+            <Sprite
+              image={'image/forest_background.jpg'}
+              width={window.innerWidth}
+              height={window.innerHeight} />)}
+          {scene === 'HOME' && (
+            <Sprite
+              image={'logo192.png'}
+              width={window.innerWidth}
+              height={window.innerHeight} />
+          )}
 
-        </Stage>
+          <Container x={400} y={330} position={[appWidth / 2, appHeight / 2]}>
+
+
+            <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]}
+            interactive={true} 
+              onclick={() => {
+                console.log('click')
+                setScene((prev) => prev === 'GAME' ?'HOME' : 'GAME')
+              }}
+              ontap={() => {
+                console.log('click')
+                setScene((prev) => prev === 'GAME' ?'HOME' : 'GAME')
+              }}
+            />
+
+          </Container>
+
+        </Stage></>
         // </AppProvider>
     );
 };
