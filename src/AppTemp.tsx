7058ef2db50d68c3f2e95a6b955ad1198e012690
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2023-03-22 16:29:05 +0700

    chore: update button

--- src/AppTemp.tsx
+++ src/AppTemp.tsx
@@ -150,18 +150,20 @@ export const AppTemp = () => {
     <div className="relative flex justify-center items-center">
       {scene === 'REGISTER' && <div className="absolute flex justify-center items-center flex-col gap-8 bg-white/50 px-3.5 py-2.5 shadow-sm hover:bg-white/80 rounded-xl ">
         <div className="flex gap-10 justify-start">
-          <button
+          <input
+            alt="btnLogin"
+            type={'image'}
+            src={'image/Btnlogin.png'}
             onClick={() => setAuthMode("LOGIN")}
-            className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-          >
-            Login
-          </button>
-          <button
+            className=" px-3.5 py-2.5 text-sm focus-visible:rounded-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+          />
+          <input
+            alt="btnRegister"
+            type={'image'}
+            src={'image/BtnRegister.png'}
             onClick={() => setAuthMode("REGISTER")}
-            className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-          >
-            Register
-          </button>
+            className=" px-3.5 py-2.5 text-sm focus-visible:rounded-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+          />
         </div>
         {authMode === "LOGIN" && (
           <>
