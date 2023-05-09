989eab8e3630d77e73d434aae764fdcdad140623
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2023-05-10 01:43:22 +0700

    chore: feedback update assets and styling

--- src/components/EggComponent.tsx
+++ src/components/EggComponent.tsx
@@ -1,187 +1,183 @@
-import React, { useEffect, useState } from 'react'
-import { eggType } from '../utils/functions'
-import { formatUnits } from 'ethers/lib/utils'
-import { Egg, useAuthStore, useStore } from '../utils/store'
-import { axiosInstance } from '../utils/api'
+import React, { useEffect, useState } from "react";
+import { eggType } from "../utils/functions";
+import { formatUnits } from "ethers/lib/utils";
+import { Egg, useAuthStore, useStore } from "../utils/store";
+import { axiosInstance } from "../utils/api";
 
 type EggComponentProps = {
-    key?: string,
-    egg: Egg,
-    index: string,
-    customTimer?: number,
-    currentTime?: number,
-    onBtnKeepPress?: () => void,
-    onBtnPurchasePress?: (idx: number) => void,
-    onBtnPayPress?: (raw: string) => void,
-}
+  key?: string;
+  egg: Egg;
+  index: string;
+  customTimer?: number;
+  currentTime?: number;
+  onBtnKeepPress?: () => void;
+  onBtnPurchasePress?: (idx: number) => void;
+  onBtnPayPress?: (raw: string) => void;
+};
 
 function EggComponent({
-    currentTime,
-    customTimer,
-    egg,
-    index,
-    onBtnKeepPress,
-    onBtnPayPress,
-    onBtnPurchasePress,
+  currentTime,
+  customTimer,
+  egg,
+  index,
+  onBtnKeepPress,
+  onBtnPayPress,
+  onBtnPurchasePress,
 }: EggComponentProps) {
+  const token = useAuthStore((state) => state.token);
+  const approved = useStore((state) => state.approved);
+  const eggTransactionData = useStore((state) => state.eggTransactionData);
+  const setEggListsData = useStore((state) => state.setEggListsData);
+  const [expiryTime, setExpiryTime] = useState(
+    customTimer ? customTimer : egg?.openat
+  );
+  // console.log('expiryTime', expiryTime)
+  // console.log('currentTime', currentTime, 'index: ', index)
+  const [countdownTime, setCountdownTime] = useState({
+    countdownHours: 0,
+    countdownMinutes: 0,
+    countdownSeconds: 0,
+  });
 
-
-    const token = useAuthStore((state) => state.token);
-    const approved = useStore((state) => state.approved);
-    const eggTransactionData = useStore((state) => state.eggTransactionData);
-    const setEggListsData = useStore((state) => state.setEggListsData);
-    const [expiryTime, setExpiryTime] = useState(customTimer ? customTimer : egg?.openat);
-    // console.log('expiryTime', expiryTime)
-    // console.log('currentTime', currentTime, 'index: ', index)
-    const [countdownTime, setCountdownTime] = useState({
-        countdownHours: 0,
-        countdownMinutes: 0,
-        countdownSeconds: 0,
+  const getEggList = async () => {
+    let options = {
+      headers: {
+        "my-auth-key": token,
+      },
+    };
+    const data: any = await axiosInstance({
+      url: "/egg/lists",
+      method: "GET",
+      headers: options.headers,
     });
+    console.log("getEggList Result:", data);
+    if (data?.status === 200 && data?.data?.result?.lists) {
+      setEggListsData(data?.data?.result);
+    }
+  };
+  useEffect(() => {
+    let timeInterval: any;
+    // const countdown = () => {
+    if (expiryTime > 0 && currentTime) {
+      // timeInterval = setInterval(() => {
+      const countdownDateTime = expiryTime * 1000;
+      // const currentTime = new Date().getTime();
+      const remainingDayTime = countdownDateTime - currentTime;
+      // console.log(`countdownDateTime ${index}`, countdownDateTime);
+      // console.log(`currentTime ${index}`, currentTime);
+      // console.log(`remainingDayTime ${index}`, remainingDayTime);
+      const totalHours = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const totalMinutes = Math.floor(
+        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
 
-    const getEggList = async () => {
-        let options = {
-            headers: {
-                "my-auth-key": token,
-            },
-        };
-        const data: any = await axiosInstance({
-            url: "/egg/lists",
-            method: "GET",
-            headers: options.headers,
-        });
-        console.log("getEggList Result:", data);
-        if (data?.status === 200 && data?.data?.result?.lists) {
-            setEggListsData(data?.data?.result);
-        }
-    };
-    useEffect(() => {
-        let timeInterval: any;
-        // const countdown = () => {
-        if (expiryTime > 0 && currentTime) {
-            // timeInterval = setInterval(() => {
-            const countdownDateTime = expiryTime * 1000;
-            // const currentTime = new Date().getTime();
-            const remainingDayTime = countdownDateTime - currentTime;
-            // console.log(`countdownDateTime ${index}`, countdownDateTime);
-            // console.log(`currentTime ${index}`, currentTime);
-            // console.log(`remainingDayTime ${index}`, remainingDayTime);
-            const totalHours = Math.floor(
-                (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-            );
-            const totalMinutes = Math.floor(
-                (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
-            );
-            const totalSeconds = Math.floor(
-                (remainingDayTime % (1000 * 60)) / 1000
-            );
+      const runningCountdownTime = {
+        countdownHours: totalHours,
+        countdownMinutes: totalMinutes,
+        countdownSeconds: totalSeconds,
+      };
 
-            const runningCountdownTime = {
-                countdownHours: totalHours,
-                countdownMinutes: totalMinutes,
-                countdownSeconds: totalSeconds,
-            };
+      if (remainingDayTime < 0) {
+        clearInterval(timeInterval);
+        setExpiryTime(0);
+        getEggList();
+      } else {
+        setCountdownTime(runningCountdownTime);
+      }
+      // }, 1000);
+    }
+    // };
+    // countdown();
+    // return () => {
+    //     clearInterval(timeInterval);
+    // };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTime]);
 
-            if (remainingDayTime < 0) {
-                clearInterval(timeInterval);
-                setExpiryTime(0);
-                getEggList()
-            } else {
-                setCountdownTime(runningCountdownTime);
-            }
-            // }, 1000);
-        }
-        // };
-        // countdown();
-        // return () => {
-        //     clearInterval(timeInterval);
-        // };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentTime]);
+  const countdownText = () => {
+    if (expiryTime === 0) return "Keep";
+    else
+      return (
+        `${
+          countdownTime.countdownHours.toString().length === 1
+            ? `0${countdownTime.countdownHours}`
+            : countdownTime.countdownHours
+        }:${
+          countdownTime.countdownMinutes.toString().length === 1
+            ? `0${countdownTime.countdownMinutes}`
+            : countdownTime.countdownMinutes
+        }:${
+          countdownTime.countdownSeconds.toString().length === 1
+            ? `0${countdownTime.countdownSeconds}`
+            : countdownTime.countdownSeconds
+        }` || ""
+      );
+  };
+  return (
+    <div key={index} className="flex flex-col items-center content-center mb-2">
+      <img
+        src="image/jurassicEggBgNew.png"
+        className="w-20 [@media(max-width:400px)]:w-[4.5rem] [@media(max-height:700px)]:w-[4.5rem]"
+        alt="jurassicEggBg"
+      />
+      <div>
+        <img
+          src={`${eggType(egg?.ticket)}`}
+          className={`w-14 -mt-[4.6rem] [@media(max-width:400px)]:w-[2.6rem] [@media(max-height:700px)]:w-[2.6rem] [@media(max-width:400px)]:-mt-[3.9rem] [@media(max-height:700px)]:-mt-[3.9rem]`}
+          alt="imgJurassicEggIcon"
+        />
+      </div>
 
+      {/* price */}
+      <div className="font magra font-bold decoration-from-font">
+        <span className="text-white text-sm [@media(max-width:400px)]:text-sm">
+          {parseFloat(
+            parseFloat(formatUnits(egg?.total, 18)).toFixed(2)
+          ).toString()}{" "}
+          USDT
+        </span>
+      </div>
 
-
-    const countdownText = () => {
-        if (expiryTime === 0) return "Keep";
-        else
-            return (
-                `${countdownTime.countdownHours.toString().length === 1
-                    ? `0${countdownTime.countdownHours}`
-                    : countdownTime.countdownHours
-                }:${countdownTime.countdownMinutes.toString().length === 1
-                    ? `0${countdownTime.countdownMinutes}`
-                    : countdownTime.countdownMinutes
-                }:${countdownTime.countdownSeconds.toString().length === 1
-                    ? `0${countdownTime.countdownSeconds}`
-                    : countdownTime.countdownSeconds
-                }` || ""
-            );
-    };
-    return (
+      {/* action button */}
+      {!currentTime ? null : expiryTime > 0 ? (
+        <div>
+          <img
+            src="image/BtnPurchaseCountdownNew.png"
+            className="w-20"
+            alt="BtnPurchaseCountdown"
+          />
+          <div className="flex w-full justify-center font magra font-bold decoration-from-font">
+            <span className="text-white -mt-[1.8rem]">{countdownText()}</span>
+          </div>
+        </div>
+      ) : (
         <div
-            key={index}
-            className="flex flex-col items-center content-center "
+          onClick={() => {
+            if (
+              approved &&
+              eggTransactionData &&
+              approved.toString() >= eggTransactionData.total &&
+              onBtnPayPress
+            ) {
+              onBtnPayPress(eggTransactionData.TxRawPayment);
+            } else if (onBtnKeepPress) onBtnKeepPress();
+          }}
         >
-            <img
-                src="image/jurassicEggBg.png"
-                className="w-24 [@media(max-width:400px)]:w-[4.5rem] [@media(max-height:700px)]:w-[4.5rem]"
-                alt="jurassicEggBg"
-            />
-            <div>
-                <img
-                    src={`${eggType(egg?.ticket)}`}
-                    className={`w-14 -mt-[5.1rem] [@media(max-width:400px)]:w-[2.6rem] [@media(max-height:700px)]:w-[2.6rem] [@media(max-width:400px)]:-mt-[3.9rem] [@media(max-height:700px)]:-mt-[3.9rem]`}
-                    alt="imgJurassicEggIcon"
-                />
-            </div>
-
-            {/* price */}
-            <div className="font magra font-bold decoration-from-font">
-                <span className="text-[#FFC700] [@media(max-width:400px)]:text-sm">
-
-                    {parseFloat(
-                        parseFloat(formatUnits(egg?.total, 18)).toFixed(2)
-                    ).toString()} USDT
-                </span>
-            </div>
-
-            {/* action button */}
-            {!currentTime ? null :
-                expiryTime > 0 ? (
-                    <div>
-                        <img
-                            src="image/BtnPurchaseCountdown.png"
-                            className="w-20 "
-                            alt="BtnPurchaseCountdown"
-                        />
-                        <div className="flex w-full justify-center font magra font-bold decoration-from-font">
-                            <span className="text-white -mt-[1.8rem]">
-                                {countdownText()}
-                            </span>
-                        </div>
-                    </div>
-                ) : (
-                    <div onClick={() => {
-                        if (approved && eggTransactionData && approved.toString() >= eggTransactionData.total && onBtnPayPress) {
-                            onBtnPayPress(eggTransactionData.TxRawPayment)
-                        }
-                        else if (onBtnKeepPress) onBtnKeepPress()
-                    }}>
-                        <img
-                            src="image/BtnPurchaseActive.png"
-                            className="w-20 "
-                            alt="BtnPurchaseActive"
-                        />
-                        <div className="flex w-full justify-center font magra font-bold decoration-from-font cursor-pointer">
-                            <span className="text-white -mt-[1.8rem]">
-                                Keep
-                            </span>
-                        </div>
-                    </div>
-                )
-            }
-        </div >
-    )
+          <img
+            src="image/BtnPurchaseActive.png"
+            className="w-20 "
+            alt="BtnPurchaseActive"
+          />
+          <div className="flex w-full justify-center font magra font-bold decoration-from-font cursor-pointer">
+            <span className="text-white -mt-[1.8rem]">Keep</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default React.memo(EggComponent)
+export default React.memo(EggComponent);
