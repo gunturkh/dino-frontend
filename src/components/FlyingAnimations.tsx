7ba2a76a405668d111907ab4d980c4c73a4e368d
Guntur KH <guntur.kh@gmail.com>
2023-06-02 20:45:16 +0700

    fix: flying dino animations

--- src/components/FlyingAnimations.tsx
+++ src/components/FlyingAnimations.tsx
@@ -139,20 +139,21 @@ function FlyingAnimations() {
         if (containerRef.current?.x) {
           // @ts-ignore
           containerRef.current.x += delta * (app.screen.width / duration);
+
+          // if containerRef x position is greater than the screen width + flyingDino1 width * 1.2
+          if (
+            // @ts-ignore
+            containerRef.current.x >
+            app.screen.width + flyingDino1.width * 1.2
+          ) {
+            // increase the counter by 1 after waiting for a few second
+            setTimeout(() => {
+              setAnimationCounter((prev) => prev + 1);
+            }, waitDuration);
+            tickerOutside.stop();
+          }
         }
 
-        // if containerRef x position is greater than the screen width + flyingDino1 width * 1.2
-        if (
-          // @ts-ignore
-          containerRef.current.x >
-          app.screen.width + flyingDino1.width * 1.2
-        ) {
-          // increase the counter by 1 after waiting for a few second
-          setTimeout(() => {
-            setAnimationCounter((prev) => prev + 1);
-          }, waitDuration);
-          tickerOutside.stop();
-        }
       });
       tickerOutside.start();
     }
@@ -172,19 +173,19 @@ function FlyingAnimations() {
         if (containerRef.current?.x) {
           // @ts-ignore
           containerRef.current.x -= delta * (app.screen.width / duration);
+          if (
+            // @ts-ignore
+            containerRef.current.x <
+            -flyingDino1.width * 1.2
+          ) {
+            setTimeout(() => {
+              setAnimationCounter((prev) => prev + 1);
+            }, waitDuration);
+            tickerOutside.stop();
+          }
         }
         // console.log("speed flyingDino", delta * (app.screen.width / duration));
 
-        if (
-          // @ts-ignore
-          containerRef.current.x <
-          -flyingDino1.width * 1.2
-        ) {
-          setTimeout(() => {
-            setAnimationCounter((prev) => prev + 1);
-          }, waitDuration);
-          tickerOutside.stop();
-        }
       });
       tickerOutside.start();
     }
@@ -202,17 +203,17 @@ function FlyingAnimations() {
         if (containerRef.current?.x) {
           // @ts-ignore
           containerRef.current.x += delta * (app.screen.width / duration);
+          // if containerRef x position is greater than the screen width + flyingDino1 width * 1.2
+          if (
+            // @ts-ignore
+            containerRef.current.x >
+            app.screen.width + flyingDino3.width * 1.2
+          ) {
+            setAnimationCounter(-1);
+            tickerOutside.stop();
+          }
         }
 
-        // if containerRef x position is greater than the screen width + flyingDino1 width * 1.2
-        if (
-          // @ts-ignore
-          containerRef.current.x >
-          app.screen.width + flyingDino3.width * 1.2
-        ) {
-          setAnimationCounter(-1);
-          tickerOutside.stop();
-        }
       });
       tickerOutside.start();
     }
